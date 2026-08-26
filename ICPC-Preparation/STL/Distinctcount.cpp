f8b31b671d3f38d12d9a289de0e6ca2b9f5fce6c
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-26 19:12:11 +0600

    Add distinct count evaluation in Distinctcount.cpp
    Implement distinct count logic to evaluate input.

---
+++ ICPC-Preparation/STL/Distinctcount.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;    //Reading input from STDIN
+	while(num--){
+		int n,x;
+		cin>>n>>x;
+
+		vector<int>v(n);
+		 for(int i=0;i<v.size();i++){
+			cin>>v[i];
+		 }
+		sort(v.begin(),v.end());
+		int sz = unique (v.begin(),v.end())-v.begin();
+		if(sz==x){
+			cout<<"Good"<<endl;
+		}else if(sz<x){
+			cout<<"Average"<<endl;
+          }else{
+            cout<<"Bad"<<endl;
+          }
+          
+}
+}
