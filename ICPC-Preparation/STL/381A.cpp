88a501ccc99354d6b394fd16c4212c390dd51935
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-26 16:25:42 +0600

    Add solution for problem 381A in C++

---
+++ ICPC-Preparation/STL/381A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int n,s=0,d=0,f1=1;
+     cin>>n;
+     vector<int>v(n);
+     for(int i=0;i<n;i++){
+         cin>>v[i];
+     }
+     while(!v.empty()){
+         if(f1==1){
+             if(v[0]>v.back()){
+                 s+=v[0];
+                 v.erase(v.begin());
+             }else{
+                 s+=v.back();
+                  v.pop_back();
+             }
+          f1=2;
+
+         }else{
+               if(v[0]>v.back()){
+                 d+=v[0];
+                 v.erase(v.begin());
+             }else{
+                 d+=v.back();
+                 v.pop_back();
+             }
+             f1=1;
+         }
+     }
+     cout<<s<<" "<<d<<endl;
+
+}
