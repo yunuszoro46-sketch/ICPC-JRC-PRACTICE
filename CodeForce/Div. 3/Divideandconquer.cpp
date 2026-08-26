491a7be813b753076a0ba41b98cb212955813239
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-26 14:16:47 +0600

    Add Divide and Conquer solution in Divideandconquer.cpp
    Implement a program to check divisibility of pairs.

---
+++ CodeForce/Div. 3/Divideandconquer.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+    int x,y;
+    for(int i=0;i<=n;i++){
+       cin>>x>>y;
+        if(x%y==0){
+          cout<<"YES"<<endl;
+        }else{
+          cout<<"NO"<<endl;
+        }
+    }
+
+   }
+
+
